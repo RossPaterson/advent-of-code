b6210756f26534ad8818e63d91b0f69d776998a4
Ross Paterson <R.Paterson@city.ac.uk>
2016-12-09 20:36:37 +0000

    cleanup

--- Day9.hs
+++ Day9.hs
@@ -4,37 +4,43 @@ import Parser
 import Control.Applicative
 import Data.Char
 
-data Marker = End | Single Char String | Marker Int String String
+data Element = Single Char | Repeat Int String
+type Input = [Element]
 
-parseString :: String -> Marker
-parseString = runParser $
-    marker <$ char '(' <*> nat <* char 'x' <*> nat <* char ')' <*> rest <|>
-    Single <$> satisfy (/= '(') <*> rest <|>
-    pure End
+parseString :: String -> Input
+parseString = runParser $ many (marker <|> single)
   where
-    marker len count s = Marker count (take len s) (drop len s)
+    marker = do
+        char '('
+        len <- nat
+        char 'x'
+        count <- nat
+        char ')'
+        str <- sequence (replicate len anyChar)
+        return (Repeat count str)
+    single = Single <$> satisfy (/= '(')
 
-expand :: String -> String
-expand s = case parseString s of
-    End -> []
-    Single c s -> c : expand s
-    Marker count prefix s -> concat (replicate count prefix) ++ expand s
+expand :: Input -> String
+expand = concatMap expandElement
+  where
+    expandElement (Single c) = [c]
+    expandElement (Repeat n s) = concat (replicate n s)
 
 solve1 :: String -> Int
-solve1 = length . expand . filter (not . isSpace)
+solve1 = length . expand . parseString . filter (not . isSpace)
 
 tests = ["ADVENT", "A(1x5)BC", "(3x3)XYZ", "A(2x2)BCD(2x2)EFG", "(6x1)(1x3)A", "X(8x2)(3x3)ABCY"]
 
 -- Part Two --
 
-expand2 :: String -> Int
-expand2 s = case parseString s of
-    End -> 0
-    Single c s -> 1 + expand2 s
-    Marker count prefix s -> count * expand2 prefix + expand2 s
+expand2 :: Input -> Int
+expand2 = sum . map expandElement
+  where
+    expandElement (Single c) = 1
+    expandElement (Repeat n s) = n * expand2 (parseString s)
 
 solve2 :: String -> Int
-solve2 = expand2 . filter (not . isSpace)
+solve2 = expand2 . parseString . filter (not . isSpace)
 
 tests2 = ["(3x3)XYZ", "X(8x2)(3x3)ABCY", "(27x12)(20x12)(13x14)(7x10)(1x12)A", "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"]
 
