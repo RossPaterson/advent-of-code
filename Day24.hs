aa6e586801d2201fb889c6f49962318953128b79
Ross Paterson <R.Paterson@city.ac.uk>
2016-12-25 00:32:18 +0000

    day 24

---
+++ Day24.hs
@@ -0,0 +1,76 @@
+module Main where
+
+import Utilities
+import Data.List
+import Data.Maybe
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+type Input = Maze
+
+data Pos = Pos Int Int
+  deriving (Show, Eq, Ord)
+type NodeName = Char
+data Maze = Maze { openPos :: Set Pos, nodeLoc :: Map NodeName Pos }
+  deriving Show
+
+mkMaze :: [String] -> Maze
+mkMaze ls = Maze {
+    openPos = Set.fromList [Pos x y |
+        (y, l) <- zip [0..] ls, (x, c) <- zip [0..] l, c /= '#'],
+    nodeLoc = Map.fromList [(c, Pos x y) |
+        (y, l) <- zip [0..] ls, (x, c) <- zip [0..] l, c /= '#' && c /= '.']
+    }
+
+neighbours :: Maze -> Pos -> [Pos]
+neighbours m (Pos x y) =
+    [p | p <- [Pos (x-1) y, Pos (x+1) y, Pos x (y-1), Pos x (y+1)],
+        Set.member p (openPos m) ]
+
+nodes :: Maze -> [NodeName]
+nodes m = Map.keys (nodeLoc m)
+
+parse :: String -> Input
+parse = mkMaze . lines
+
+-- distances between nodes within the maze
+type Graph = Map (NodeName, NodeName) Int
+
+mkGraph :: Maze -> Graph
+mkGraph m =
+    Map.fromList [((src, dest), dist) |
+        src <- nodes m,
+        (dist, ns) <- zip [0..] (bfs (neighbours m) [nodeLoc m!src]),
+        n <- ns,
+        dest <- maybeToList (Map.lookup n nodeOf)]
+  where
+    nodeOf = Map.fromList [(p, n) | (n, p) <- Map.assocs (nodeLoc m)]
+
+startNode :: NodeName
+startNode = '0'
+
+solve1 :: Input -> Int
+solve1 m = minimum (map cost (permutations other_ns))
+  where
+    cost path = sum [g!e | e <- zip (startNode:path) path]
+    other_ns = filter (/= startNode) (nodes m)
+    g = mkGraph m
+
+-- Part Two --
+
+solve2 :: Input -> Int
+solve2 m = minimum (map cost (permutations other_ns))
+  where
+    cost path = sum [g!e | e <- zip (startNode:path) path] +
+        g!(last path, startNode)
+    other_ns = filter (/= startNode) (nodes m)
+    g = mkGraph m
+
+main :: IO ()
+main = do
+    s <- readFile "input24.txt"
+    let input = parse s
+    print (solve1 input)
+    print (solve2 input)
