5a3e93303559f81cd0c2ee4ee5e442673c3d8883
Ross Paterson <R.Paterson@city.ac.uk>
2016-12-31 03:19:48 +0000

    day 9

---
+++ Day09.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import Parser
+import Control.Applicative
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Char
+import Data.List
+import Data.Maybe
+
+type Node = String
+type Input = Map (Node, Node) Int
+
+parse :: String -> Input
+parse = Map.fromList . map (runParser dist) . lines
+  where
+    dist = (,) <$> edge <* string " = " <*> nat
+    edge = (,) <$> node <* string " to " <*> node
+    node = some letter
+
+getDist :: Input -> Node -> Node -> Int
+getDist m src dest =
+    fromMaybe maxBound (Map.lookup (src, dest) m <|> Map.lookup (dest, src) m)
+
+nodes :: Input -> [Node]
+nodes m = Set.toList $ Set.fromList $
+    concat [[src, dest] | (src, dest) <- Map.keys m]
+
+solve1 :: Input -> Int
+solve1 m =
+    minimum [sum (zipWith (getDist m) ns (tail ns)) |
+        ns <- permutations (nodes m)]
+
+-- Part Two --
+
+solve2 :: Input -> Int
+solve2 m =
+    maximum [sum (zipWith (getDist m) ns (tail ns)) |
+        ns <- permutations (nodes m)]
+
+main :: IO ()
+main = do
+    s <- readFile "input09.txt"
+    let input = parse s
+    print (solve1 input)
+    print (solve2 input)
