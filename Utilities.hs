48360796fa00df78b1b2432f8d022b708f41b1d3
Ross Paterson <R.Paterson@city.ac.uk>
2017-01-03 15:49:27 +0000

    add allValues and whileJust

--- Utilities.hs
+++ Utilities.hs
@@ -3,6 +3,13 @@ module Utilities where
 import Data.List
 import qualified Data.Set as Set
 
+allValues :: (Bounded a, Enum a) => [a]
+allValues = [minBound..maxBound]
+
+-- repeatedly apply the function until it doesn't produce a new value
+whileJust :: (a -> Maybe a) -> a -> a
+whileJust f x = maybe x (whileJust f) (f x)
+
 -- select all the elements of xs that have the least value of f
 leastBy :: (Ord v) => (a -> v) -> [a] -> [a]
 leastBy f = head . groupBy same . sortOn f
