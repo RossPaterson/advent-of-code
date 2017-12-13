b488b9ef2c4b4b51ba4331618eb8d5ae285a492f
Ross Paterson <R.Paterson@city.ac.uk>
2017-12-13 10:20:23 +0000

    rename to match problem statement

--- Day13.hs
+++ Day13.hs
@@ -5,29 +5,29 @@ import Utilities
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-type Scanner = (Int, Int)
-type Wall = [Scanner]
+type Layer = (Int, Int)
+type Wall = [Layer]
 type Input = Wall
 
 parse :: String -> Input
-parse = map (runParser scanner) . lines
+parse = map (runParser layer) . lines
   where
-    scanner = (,) <$> nat <* string ": " <*> nat
+    layer = (,) <$> nat <* string ": " <*> nat
 
--- Scanners that are in the start position when a packet started after delay
--- arrives at their depth.
--- The period of a back-and-forth scanner with range r is 2*(r-1)
-catches :: Int -> Wall -> [Scanner]
-catches delay = filter catch
+-- Layers whose scanners are in the start position when a packet started
+-- after delay arrives at their depth.
+catches :: Int -> Wall -> [Layer]
+catches delay = filter caught
   where
-    catch :: Scanner -> Bool
-    catch (d, r) = (d + delay) `mod` (2*(r-1)) == 0
+    -- The period of a back-and-forth scanner with range r is 2*(r-1)
+    caught :: Layer -> Bool
+    caught (d, r) = (d + delay) `mod` (2*(r-1)) == 0
 
-cost :: Scanner -> Int
-cost (d, r) = d*r
+severity :: Layer -> Int
+severity (d, r) = d*r
 
 solve1 :: Input -> Int
-solve1 = sum . map cost . catches 0
+solve1 = sum . map severity . catches 0
 
 testInput :: String
 testInput =
@@ -41,11 +41,11 @@ tests1 = [(testInput, 24)]
 
 -- Part Two
 
-shortestDelay :: Input -> Int
-shortestDelay wall = head [delay | delay <- [0..], null (catches delay wall)]
+safeDelays :: Input -> [Int]
+safeDelays wall = [delay | delay <- [0..], null (catches delay wall)]
 
 solve2 :: Input -> Int
-solve2 = shortestDelay
+solve2 = head . safeDelays
 
 tests2 :: [(String, Int)]
 tests2 = [(testInput, 10)]
