6ad2a8b4fc0a7707ee0fb4c36d613e13e7ee5864
Ross Paterson <R.Paterson@city.ac.uk>
2017-12-14 10:53:41 +0000

    factor out knothash for later use

--- Day10.hs
+++ Day10.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Knothash
 import Utilities
 import Data.Bits
 import Data.Char
@@ -11,43 +12,8 @@ type Input = [Int]
 parse :: String -> Input
 parse cs = map read (words [if c == ',' then ' ' else c | c <- cs, c /= '\n'])
 
--- the list of numbers is rotated by position
-data State = State { list :: [Int], position :: Int, skipSize :: Int }
-    deriving Show
-
--- the list of numbers in their original positions
-numbers :: State -> [Int]
-numbers (State ns pos _) = back ++ front
-  where
-    (front, back) = splitAt (length ns - pos) ns
-
-initState :: Int -> State
-initState size = State [0..size-1] 0 0
-
--- apply f to the next n elements of the ring, and advance to the end of that
-advance :: Int -> ([Int] -> [Int]) -> State -> State
-advance n f (State ns pos skip) = State ns' pos' skip
-  where
-    (front, back) = splitAt n ns
-    ns' = back ++ f front
-    pos' = (pos + n) `mod` size
-    size = length ns
-
-incrSkip :: State -> State
-incrSkip (State ns pos skip) = State ns pos ((skip+1) `mod` length ns)
-
-{-
-One step of the hash:
-- Reverse the order of that length of elements in the list, starting
-  with the element at the current position.
-- Move the current position forward by that length plus the skip size.
-- Increase the skip size by one.
--}
-step :: State -> Int -> State
-step s len = incrSkip $ advance (skipSize s) id $ advance len reverse s
-
 hash1 :: Int -> [Int] -> [Int]
-hash1 size = numbers . foldl step (initState size)
+hash1 = hashRounds 1
 
 solve1 :: Input -> Int
 solve1 = product . take 2 . hash1 256
@@ -58,11 +24,8 @@ tests1 = [((5, [3, 4, 1, 5]), [3, 4, 2, 1, 0])]
 -- Part Two
 
 solve2 :: String -> String
-solve2 s = hex $ dense $ numbers $ times 64 round $ initState 256
+solve2 = hex . knothash 
   where
-    round s = foldl step s lengths
-    lengths = map ord (filter (/= '\n') s) ++ [17, 31, 73, 47, 23]
-    dense = map (foldr1 xor) . groups 16
     hex = concatMap hexDigit
     hexDigit n
       | n < 16 = "0" ++ showHex n ""
