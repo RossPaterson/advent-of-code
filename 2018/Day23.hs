d765771f569c6f4af1ccf6dd0f54ce0c2669757c
Ross Paterson <R.Paterson@city.ac.uk>
2018-12-28 22:34:26 +0000

    tweak terminology in comments

--- 2018/Day23.hs
+++ 2018/Day23.hs
@@ -11,26 +11,30 @@ import Data.Ord
 
 type Input = System
 
-type System = [Nanobot]
-data Nanobot = Nanobot { pos :: Position, radius :: Int }
+type System = [Octahedron]
+
+-- The set of points whose Manhattan distance from the position of a
+-- nanobot is no more than the radius is a regular octahedron with its
+-- vertices in the axis directions.
+data Octahedron = Octahedron { centre :: Position, radius :: Int }
   deriving Show
 type Position = (Int, Int, Int)
 
 parse :: String -> Input
-parse = map (runParser nanobot) . lines
+parse = map (runParser octahedron) . lines
   where
-    nanobot = Nanobot <$ string "pos=" <*> point <*
+    octahedron = Octahedron <$ string "pos=" <*> point <*
         string ", r=" <*> nat
     point = (,,) <$ char '<' <*> int <* char ',' <*> int <*
         char ',' <*> int <* char '>'
 
 -- Part One
 
--- number of nanobots within range of the one with the largest radius
+-- number of centres within range of the largest octahedron
 solve1 :: Input -> Int
-solve1 ns = length [n | n <- ns, distance (pos n) p <= r]
+solve1 ns = length [n | n <- ns, distance (centre n) p <= r]
   where
-    Nanobot p r = maximumBy (comparing radius) ns
+    Octahedron p r = maximumBy (comparing radius) ns
 
 -- Manhattan distance
 distance :: Position -> Position -> Int
@@ -52,17 +56,17 @@ testInput1 = "\
 
 -- Part Two
 
--- shortest distance from the origin of points that are within range of
--- the most nanobots.
+-- shortest distance from the origin of points that are within the
+-- most octahedrons.
 -- Uses a directed search of cubes of candidate points, starting with a
--- cube containing all the nanobot ranges, and at each stage examining
--- the code that intersects with the most nanobot ranges and dividing
--- it into eight subcubes for further consideration.
-solve2 :: [Nanobot] -> Int
+-- cube containing all the octahedrons, and at each stage examining the
+-- code that intersects with the most octahedrons and dividing it into
+-- eight subcubes for further consideration.
+solve2 :: [Octahedron] -> Int
 solve2 ns = searchCubes (add (boundingCube ns) PQ.empty)
   where
-    -- The ordering on keys ensures that if the least is a cube of size 1,
-    -- it is a point inside the most nanobot ranges that has the least
+    -- The ordering on keys ensures that if the least is a cube of size
+    -- 1, it is a point inside the most octahedrons that has the least
     -- distance to the origin.
     searchCubes pq = case PQ.extract pq of
         Nothing -> error "empty queue"
@@ -77,16 +81,16 @@ solve2 ns = searchCubes (add (boundingCube ns) PQ.empty)
 data Cube = Cube Position Int
   deriving Show
 
--- cube containing all the ranges of the nanobots
-boundingCube :: [Nanobot] -> Cube
+-- cube containing all the octahedrons
+boundingCube :: [Octahedron] -> Cube
 boundingCube ps = Cube (xmin, ymin, zmin) size
   where
-    xmin = minimum [x-r | Nanobot (x, y, z) r <- ps]
-    xmax = maximum [x+r | Nanobot (x, y, z) r <- ps]
-    ymin = minimum [y-r | Nanobot (x, y, z) r <- ps]
-    ymax = maximum [y+r | Nanobot (x, y, z) r <- ps]
-    zmin = minimum [z-r | Nanobot (x, y, z) r <- ps]
-    zmax = maximum [z+r | Nanobot (x, y, z) r <- ps]
+    xmin = minimum [x-r | Octahedron (x, y, z) r <- ps]
+    xmax = maximum [x+r | Octahedron (x, y, z) r <- ps]
+    ymin = minimum [y-r | Octahedron (x, y, z) r <- ps]
+    ymax = maximum [y+r | Octahedron (x, y, z) r <- ps]
+    zmin = minimum [z-r | Octahedron (x, y, z) r <- ps]
+    zmax = maximum [z+r | Octahedron (x, y, z) r <- ps]
     maxdim = ((xmax - xmin) `max` (ymax - ymin) `max` (xmax - zmin)) + 1
     size = head $ dropWhile (< maxdim) $ iterate (*2) 1
 
@@ -98,13 +102,14 @@ splitCube (Cube (x, y, z) s) =
   where
     s2 = s `div` 2
 
--- maximum number of nanobots within range of any point in the cube
-numIntersects :: [Nanobot] -> Cube -> Int
+-- number of octahedrons containing any point in the cube
+numIntersects :: [Octahedron] -> Cube -> Int
 numIntersects ns c = length [n | n <- ns, intersectCube n c]
 
--- does the nanobot's range share any points with the cube?
-intersectCube :: Nanobot -> Cube -> Bool
-intersectCube (Nanobot (cx, cy, cz) r) (Cube (x, y, z) s) = dx + dy + dz <= r
+-- does the octahedron share any points with the cube?
+intersectCube :: Octahedron -> Cube -> Bool
+intersectCube (Octahedron (cx, cy, cz) r) (Cube (x, y, z) s) =
+    dx + dy + dz <= r
   where
     dx = 0 `max` (x - cx) `max` (cx - (x + s - 1))
     dy = 0 `max` (y - cy) `max` (cy - (y + s - 1))
@@ -117,7 +122,7 @@ data Key = Key (Down Int) (Down Int) Int
   deriving (Show, Eq, Ord)
 
 -- priority key for a search cube
-makeKey :: [Nanobot] -> Cube -> Key
+makeKey :: [Octahedron] -> Cube -> Key
 makeKey ns c@(Cube p s) =
     Key (Down (numIntersects ns c)) (Down (distance p (0,0,0))) s
 
