4db1be1eb7a96325c53e9b794dd856482ad6ea2b
Ross Paterson <R.Paterson@city.ac.uk>
2018-12-29 16:12:25 +0000

    split priority queue into min and max versions

--- utilities/MaxPriorityQueue.hs
+++ utilities/MaxPriorityQueue.hs
@@ -1,11 +1,12 @@
-module PriorityQueue where
+module MaxPriorityQueue where
 
--- Lazy skew heaps, from "Fun with Binary Heap Trees" by Chris Okasaki
--- in "The Fun of Programming", pp 1-16.
+-- Maximum priority queue, implemented with lazy skew heaps, from
+-- "Fun with Binary Heap Trees" by Chris Okasaki in
+-- "The Fun of Programming", pp 1-16.
 
 data PQ p a = Null | Fork p a (PQ p a) (PQ p a)
 
--- extract the element with the least key
+-- extract the element with the greatest key
 extract :: Ord p => PQ p a -> Maybe (p, a, PQ p a)
 extract Null = Nothing
 extract (Fork p x a b) = Just (p, x, merge a b)
@@ -27,5 +28,5 @@ merge :: Ord p => PQ p a -> PQ p a -> PQ p a
 merge a Null = a
 merge Null b = b
 merge a@(Fork ap ax aa ab) b@(Fork bp bx ba bb)
-  | ap <= bp  = Fork ap ax ab (merge aa b)
+  | ap >= bp  = Fork ap ax ab (merge aa b)
   | otherwise = Fork bp bx bb (merge ba a)
