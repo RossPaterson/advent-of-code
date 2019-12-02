adbaa1f22650dd5bcd02828ec439bf06532fb06f
Ross Paterson <R.Paterson@city.ac.uk>
2019-12-02 17:19:17 +0000

    day 2

---
+++ 2019/Day02.hs
@@ -0,0 +1,48 @@
+module Main where
+
+import Intcode
+import Utilities
+import Data.List
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+
+-- Input processing
+
+type Input = Memory
+
+parse :: String -> Input
+parse = readMemory
+
+-- Part One
+
+initialize :: Int -> Int -> Memory -> Memory
+initialize n v = Map.insert 1 n . Map.insert 2 v
+
+solve1 :: Input -> Int
+solve1 = output . run . initialize 12 2
+
+tests1 :: [(String, [Int])]
+tests1 = [
+    ("1,9,10,3,2,3,11,0,99,30,40,50", [3500,9,10,70,2,3,11,0,99,30,40,50]),
+    ("1,0,0,0,99", [2,0,0,0,99]),
+    ("2,3,0,3,99", [2,3,0,6,99]),
+    ("2,4,4,5,99,0", [2,4,4,5,99,9801]),
+    ("1,1,1,4,99,5,6,0,99", [30,1,1,4,2,5,6,0,99])]
+
+-- Part Two
+
+target2 :: Int
+target2 = 19690720
+
+solve2 :: Input -> Int
+solve2 mem = head [100 * noun + verb | noun <- [0..99], verb <- [0..99],
+    output (run (initialize noun verb mem)) == target2]
+
+main :: IO ()
+main = do
+    s <- readFile "input/02.txt"
+    let input = parse s
+    putStr (unlines (failures "solve1" (Map.elems . run . parse) tests1))
+    print (solve1 input)
+    putStr $ unlines $ map show $ trace $ initialize 98 20 input
+    print (solve2 input)
--- 2019/Notes.md
+++ 2019/Notes.md
@@ -8,3 +8,22 @@
 
 Another simple one to start with.  The first part is a trivial calculation,
 which is iterated in the second part.
+
+## [Day 2: 1202 Program Alarm](https://adventofcode.com/2019/day/2)
+
+Introducing this year's assembly language.  The first part is straightforward
+implementation of the machine.  The second part is an inversion, for which
+exhaustive search was sufficient.
+
+On later examination, the code turns out to be relatively simple, not
+modifying any instructions that are executed later.  The second and third
+instructions have no effect, but are there to provide a range of constants
+to be added or multiplied to the first input value, to which the second
+input value is then added, so it computes A*x + y + B for some constants
+A and B.
+
+We are told the machine will be used later, with more instructions,
+presumably including one to set the instruction pointer from a value in
+memory.  It is a stored-program computer that executes self-modifying
+programs with instructions of different lengths, so it's going to
+be tricky.
