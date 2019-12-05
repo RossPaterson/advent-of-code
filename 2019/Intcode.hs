d2577fb86ae3cc622b453f79cd4ed836a7c17265
Ross Paterson <R.Paterson@city.ac.uk>
2019-12-05 19:10:57 +0000

    cleanup

--- 2019/Intcode.hs
+++ 2019/Intcode.hs
@@ -7,8 +7,9 @@ import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Maybe
 
-type Addr = Int
-type Memory = Map Addr Int
+type Memory = Map Address Value
+type Address = Int
+type Value = Int
 
 -- comma-separated list of values for locations 0..
 readMemory :: String -> Memory
@@ -16,30 +17,30 @@ readMemory s = Map.fromAscList $ zip [0..] $
     map read (words [if c == ',' then ' ' else c | c <- s])
 
 data State = State {
-    curr_ip :: Addr,
     memory :: Memory,
-    inputs :: [Int], -- unread input values
-    outputs :: [Int] -- output values in reverse order
+    curr_ip :: Address,
+    inputs :: [Value], -- unread input values
+    outputs :: [Value] -- output values in reverse order
     }
     deriving Show
 
-initState :: [Int] -> Memory -> State
+initState :: [Value] -> Memory -> State
 initState vs mem = State {
-    curr_ip = 0,
     memory = mem,
+    curr_ip = 0,
     inputs = vs,
     outputs = []
     }
 
-data Modification
-    = Set Addr Int
-    | ReadTo Addr
-    | Write Int
-    | SetIP Int
+data Action
+    = Set Address Value
+    | ReadTo Address
+    | Write Value
+    | SetIP Value
     | Nop
     deriving Show
 
-apply :: Modification -> State -> State
+apply :: Action -> State -> State
 apply (Set addr v) s = s { memory = Map.insert addr v (memory s) }
 apply (Write v) s = s { outputs = v:outputs s }
 apply (ReadTo addr) s = case (inputs s) of
@@ -49,26 +50,23 @@ apply (SetIP addr) s = s { curr_ip = addr }
 apply Nop s = s
 
 data Instruction
-    = Add Parameter Parameter Addr -- Day 2
-    | Mul Parameter Parameter Addr -- Day 2
-    | Input Addr -- Day 5 part 1
+    = Add Parameter Parameter Address -- Day 2
+    | Mul Parameter Parameter Address -- Day 2
+    | Input Address -- Day 5 part 1
     | Output Parameter -- Day 5 part 1
     | JumpIfTrue Parameter Parameter -- Day 5 part 2
     | JumpIfFalse Parameter Parameter -- Day 5 part 2
-    | LessThan Parameter Parameter Addr -- Day 5 part 2
-    | Equals Parameter Parameter Addr -- Day 5 part 2
+    | LessThan Parameter Parameter Address -- Day 5 part 2
+    | Equals Parameter Parameter Address -- Day 5 part 2
     | Halt -- Day 2
     deriving Show
 
-data Parameter = Parameter ParameterMode Int
+-- instruction parameter
+data Parameter = Position Address | Immediate Value
     deriving Show
 
--- addressing modes for parameters
-data ParameterMode = Position | Immediate
-    deriving (Enum, Show)
-
 -- returns decoded instruction and location of following instruction
-fetch :: State -> (Instruction, Addr)
+fetch :: State -> (Instruction, Address)
 fetch s = case leadvalue `mod` 100 of
     1 -> (Add (parameter 1) (parameter 2) (arg 3), ip+4)
     2 -> (Mul (parameter 1) (parameter 2) (arg 3), ip+4)
@@ -84,13 +82,16 @@ fetch s = case leadvalue `mod` 100 of
     mem = memory s
     ip = curr_ip s
     leadvalue = mem_lookup "out of range" ip
-    parameter n = Parameter (mode n) (arg n)
+    parameter n = mode n (arg n)
     arg n = mem_lookup ("parameter " ++ show n ++ " out of range") (ip+n)
-    mode n = toEnum (leadvalue `div` (10^(n+1)) `mod` 10)
+    mode n v = case leadvalue `div` (10^(n+1)) `mod` 10 of
+        0 -> Position v
+        1 -> Immediate v
+        d -> exec_error $ "bad parameter mode " ++ show d
     mem_lookup msg addr = fromMaybe (exec_error msg) (Map.lookup addr mem)
     exec_error msg = error (show ip ++ ": " ++ msg)
 
-effect :: Instruction -> Memory -> Maybe Modification
+effect :: Instruction -> Memory -> Maybe Action
 effect instr mem = case instr of
     Add x y r -> Just (Set r (get x + get y))
     Mul x y r -> Just (Set r (get x * get y))
@@ -102,10 +103,10 @@ effect instr mem = case instr of
     Equals x y r -> Just (Set r (fromEnum (get x == get y)))
     Halt -> Nothing
   where
-    get (Parameter Position addr) =
+    get (Position addr) =
         fromMaybe (error ("address " ++ show addr ++ " out of range"))
             (Map.lookup addr mem)
-    get (Parameter Immediate n) = n
+    get (Immediate n) = n
 
 -- fetch-execute cycle
 advance :: State -> Maybe State
@@ -118,7 +119,7 @@ advance s = do
 run :: Memory -> Memory
 run = fst . runIO []
 
-runIO :: [Int] -> Memory -> (Memory, [Int])
+runIO :: [Value] -> Memory -> (Memory, [Value])
 runIO vs mem = (memory s, reverse (outputs s))
   where
     s = whileJust advance (initState vs mem)
@@ -130,13 +131,13 @@ showState s = unwords (map show f) ++ "." ++ unwords (map show b)
   where
     (f, b) = splitAt (curr_ip s) (Map.elems (memory s))
 
-trace :: Memory -> [(Int, Instruction, Modification)]
+trace :: Memory -> [(Address, Instruction, Action)]
 trace = traceIO []
 
-traceIO :: [Int] -> Memory -> [(Int, Instruction, Modification)]
+traceIO :: [Value] -> Memory -> [(Address, Instruction, Action)]
 traceIO vs mem = unfoldr advanceTrace (initState vs mem)
 
-advanceTrace :: State -> Maybe ((Int, Instruction, Modification), State)
+advanceTrace :: State -> Maybe ((Address, Instruction, Action), State)
 advanceTrace s = do
     let (instr, next_ip) = fetch s
     mod <- effect instr (memory s)
@@ -144,5 +145,5 @@ advanceTrace s = do
 
 -- Run the program with several inputs and return those for which it
 -- produces different outputs than the function.
-testProgram :: Memory -> (Int -> Int) -> [Int] -> [Int]
+testProgram :: Memory -> (Value -> Value) -> [Value] -> [Value]
 testProgram mem f xs = [x | x <- xs, snd (runIO [x] mem) /= [f x]]
