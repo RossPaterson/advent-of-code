63696713f6176cb8ac00c92197728f2e47de70b1
Ross Paterson <R.Paterson@city.ac.uk>
2019-12-21 22:46:26 +0000

    trim

--- 2019/Day21.hs
+++ 2019/Day21.hs
@@ -27,7 +27,7 @@ solve1 mem = last $ streamFunction mem $ map toValue springProg1
 
 -- Part Two
 
--- This works, but not sure if it's complete
+-- This works, but not sure if it's complete:
 -- NOT (A AND B AND C) AND NOT D AND (E OR H)
 springProg2 :: String
 springProg2 =
@@ -40,22 +40,6 @@ springProg2 =
     \NOT T T\n\
     \OR H T\n\
     \AND T J\n\
-    \RUN\n"
-
--- This also works
--- NOT (A AND B AND C) AND NOT D AND ((E AND (F OR I)) OR H)
-springProg2a :: String
-springProg2a =
-    "OR F J\n\
-    \OR I J\n\
-    \AND E J\n\
-    \OR H J\n\
-    \OR A T\n\
-    \AND B T\n\
-    \AND C T\n\
-    \NOT T T\n\
-    \AND T J\n\
-    \AND D J\n\
     \RUN\n"
 
 solve2 :: Input -> Integer
