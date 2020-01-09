af7ea5972c4aed522c11d77a143e7331d81e39ba
Ross Paterson <R.Paterson@city.ac.uk>
2020-01-09 18:48:45 +0000

    simplify

--- utilities/PrioritySearchQueue.hs
+++ utilities/PrioritySearchQueue.hs
@@ -3,47 +3,40 @@ module PrioritySearchQueue (
     PSQ, empty, singleton, insert, extract
     ) where
 
-import Data.Map (Map, (!))
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 -- | A map in which the values are priorities, with an 'extract' operation
 -- returning the lowest priority.
-data PSQ k p  = PSQ (Map k p) (Map p (Set k))
+data PSQ k p  = PSQ (Map k p) (Set (p, k))
     deriving Show
 
 -- | Empty priority search queue.
 empty :: PSQ k p
-empty = PSQ Map.empty Map.empty
+empty = PSQ Map.empty Set.empty
 
 -- | A key and its priority.
 singleton :: (Ord p, Ord k) => k -> p -> PSQ k p
-singleton k p = PSQ (Map.singleton k p) (Map.singleton p (Set.singleton k))
+singleton k p = PSQ (Map.singleton k p) (Set.singleton (p, k))
 
--- | Insert a new key and priority in the queue.  If the key is already in
--- the queue, the associated priority is replaced with the supplied value.
+-- | Insert a new key and priority into the queue.  If the key is already
+-- in the queue with a greater priority, the associated priority is
+-- replaced with the supplied value.
 insert :: (Ord k, Ord p) => k -> p -> PSQ k p -> PSQ k p
 insert k p pq@(PSQ priority queue) =
     case Map.lookup k priority of
-        Nothing -> PSQ (Map.insert k p priority) queue'
+        Nothing ->
+            PSQ (Map.insert k p priority) (Set.insert (p, k) queue)
         Just p'
           | p' <= p -> pq
           | otherwise ->
-            PSQ (Map.insert k p priority) (removeEntry k p' queue')
-  where
-    queue' = Map.insertWith Set.union p (Set.singleton k) queue
-
-removeEntry :: (Ord k, Ord p) => k -> p -> Map p (Set k) -> Map p (Set k)
-removeEntry k p m
-    | Set.null entries = Map.delete p m
-    | otherwise = Map.insert p entries m
-  where
-    entries = Set.delete k (m!p)
+            PSQ (Map.insert k p priority)
+                (Set.insert (p, k) (Set.delete (p', k) queue))
 
 -- | Extract the key with lowest priority from the queue.
 extract :: (Ord k, Ord p) => PSQ k p -> Maybe (k, p, PSQ k p)
 extract (PSQ priority queue) = do
-    ((p, ks), _) <- Map.minViewWithKey queue
-    (k, _) <- Set.minView ks
-    Just (k, p, PSQ (Map.delete k priority) (removeEntry k p queue))
+    ((p, k), queue') <- Set.minView queue
+    Just (k, p, PSQ (Map.delete k priority) queue')
