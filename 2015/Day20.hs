8ff8ed4e138121a1eb3ccce9275a69d100b51020
Ross Paterson <R.Paterson@city.ac.uk>
2020-07-13 11:34:04 +0100

    update number theory functions

--- 2015/Day20.hs
+++ 2015/Day20.hs
@@ -6,15 +6,15 @@ input :: Int
 input = 29000000
 
 solve1 :: Int
-solve1 = head [n | n <- [1..], 10*sumOfFactors n >= input]
+solve1 = head [n | n <- [1..], 10*sumOfDivisors n >= toInteger input]
 
 -- Part Two --
 
-bigFactors :: Int -> [Int]
-bigFactors n = [n `div` k | k <- [1..min n 50], n `mod` k == 0]
+bigDivisors :: Int -> [Int]
+bigDivisors n = [n `div` k | k <- [1..min n 50], n `mod` k == 0]
 
 solve2 :: Int
-solve2 = head [n | n <- [1..], 11*sum (bigFactors n) >= input]
+solve2 = head [n | n <- [1..], 11*sum (bigDivisors n) >= input]
 
 main :: IO ()
 main = do
