539568f5dd88828e825888ed5ad87defe74c97d4
Ross Paterson <R.Paterson@city.ac.uk>
2020-12-05 12:41:24 +0000

    fix tests

--- 2020/Day05.hs
+++ 2020/Day05.hs
@@ -21,15 +21,16 @@ fromBits bs = sum [v | (b, v) <- zip (reverse bs) (iterate (*2) 1), b]
 
 -- Part One
 
-solve1 :: Input -> Int
-solve1 = maximum
-
+-- tests for seatID
 tests1 :: [(String, Int)]
 tests1 = [
     ("FBFBBFFRLR", 357),
     ("BFFFBBFRRR", 567),
     ("FFFBBBFRRR", 119),
     ("BBFFBBFRLL", 820)]
+
+solve1 :: Input -> Int
+solve1 = maximum
 
 -- Part Two
 
@@ -47,6 +48,6 @@ main :: IO ()
 main = do
     s <- readFile "input/05.txt"
     let input = parse s
-    putStr (unlines (failures "solve1" (solve1 . parse) tests1))
+    putStr (unlines (failures "solve1" seatID tests1))
     print (solve1 input)
     print (solve2 input)
