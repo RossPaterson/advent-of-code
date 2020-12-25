9091265ea0c92acc4e84521da4aa5c25ab799ad4
Ross Paterson <R.Paterson@city.ac.uk>
2020-12-25 23:48:41 +0000

    merge Assembly onto Day08

--- 2020/Day08.hs
+++ 2020/Day08.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Assembly
+import Parser
 import Utilities
+import Control.Applicative
 import Data.Maybe
+import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
@@ -10,10 +12,51 @@ import qualified Data.Set as Set
 
 type Input = Program
 
+type Program = Map Int Instruction
+
+data Instruction
+    = Acc Int
+    | Jmp Int
+    | Nop Int
+    deriving Show
+
 parse :: String -> Input
-parse = parseProgram
+parse = Map.fromList . zip [0..] . map (runParser instruction) . lines
+  where
+    instruction =
+        Acc <$ string "acc " <*> arg <|>
+        Jmp <$ string "jmp " <*> arg <|>
+        Nop <$ string "nop " <*> arg
+    arg = char '+' *> nat <|> negate <$ char '-' <*> nat
 
 -- Part One
+
+data State = State {
+    accumulator :: Int,
+    pc :: Int
+    }
+    deriving Show
+
+initState :: State
+initState = State 0 0
+
+-- execute a single instruction
+action :: State -> Instruction -> State
+action s (Acc n) = s { accumulator = accumulator s + n, pc = pc s + 1 }
+action s (Jmp n) = s { pc = pc s + n }
+action s (Nop _) = s { pc = pc s + 1 }
+
+-- a single execution step
+step :: Program -> State -> Maybe State
+step p s = fmap (action s) $ Map.lookup (pc s) p
+
+-- all states until the program terminates
+states :: Program -> [State]
+states p = iterateWhileJust (step p) initState
+
+-- run the program to completion
+runProgram :: Program -> State
+runProgram p = whileJust (step p) initState
 
 -- states from the first time the pc repeats
 from_first_rep :: Program -> [State]
