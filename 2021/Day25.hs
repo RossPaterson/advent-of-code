628db27ae8655f1587e68cb2086b98e78b60ee0e
Ross Paterson <R.Paterson@city.ac.uk>
2021-12-26 15:29:59 +0000

    cleanup

--- 2021/Day25.hs
+++ 2021/Day25.hs
@@ -8,8 +8,8 @@ import qualified Data.Set as Set
 -- Input processing
 
 data State = State {
-    south :: Set Position,
     east :: Set Position,
+    south :: Set Position,
     width :: Int,
     height :: Int }
     deriving Show
@@ -18,8 +18,8 @@ type Input = State
 
 parse :: String -> Input
 parse s = State {
-    south = Set.fromList [p | (p, c) <- pcs, c == 'v'],
     east = Set.fromList [p | (p, c) <- pcs, c == '>'],
+    south = Set.fromList [p | (p, c) <- pcs, c == 'v'],
     width = maximum [x | (Position x _, _) <- pcs] + 1,
     height = maximum [y | (Position _ y, _) <- pcs] + 1 }
   where
@@ -27,40 +27,48 @@ parse s = State {
 
 -- Part One
 
+setEast :: State -> Set Position -> State
+setEast s ps = s { east = ps }
+
+setSouth :: State -> Set Position -> State
+setSouth s ps = s { south = ps }
+
 moveEast :: State -> Position -> Position
 moveEast s (Position x y) = Position ((x+1) `mod` width s) y
 
 moveSouth :: State -> Position -> Position
 moveSouth s (Position x y) = Position x ((y+1) `mod` height s)
 
-allMoves :: State -> (State -> Position -> Position) -> Set Position ->
-    Maybe (Set Position)
-allMoves s move ps
+moveSet :: State -> (State -> Position -> Position) ->
+    Set Position -> Maybe (Set Position)
+moveSet s movePos ps
   | null moves = Nothing
   | otherwise = Just (Set.union new (Set.difference ps old))
   where
     moves = [(p, p') |
         p <- Set.elems ps,
-        let p' = move s p,
+        let p' = movePos s p,
         not (Set.member p' (east s)),
         not (Set.member p' (south s))]
     old = Set.fromList (map fst moves)
     new = Set.fromList (map snd moves)
 
 moveHerdEast :: State -> Maybe State
-moveHerdEast s =
-    fmap (\ ps -> s { east = ps }) (allMoves s moveEast (east s))
+moveHerdEast s = fmap (setEast s) (moveSet s moveEast (east s))
 
 moveHerdSouth :: State -> Maybe State
-moveHerdSouth s =
-    fmap (\ ps -> s { south = ps }) (allMoves s moveSouth (south s))
+moveHerdSouth s = fmap (setSouth s) (moveSet s moveSouth (south s))
+
+-- apply two possible changes in sequence
+andThen :: (a -> Maybe a) -> (a -> Maybe a) -> a -> Maybe a
+andThen f g x = case f x of
+    Nothing -> g x
+    Just y -> case g y of
+        Nothing -> Just y
+        Just z -> Just z
 
 step :: State -> Maybe State
-step s = case moveHerdEast s of
-    Nothing -> moveHerdSouth s
-    Just s' -> case moveHerdSouth s' of
-        Nothing -> Just s'
-        Just s'' -> Just s''
+step = moveHerdEast `andThen` moveHerdSouth
 
 solve1 :: Input -> Int
 solve1 = length . iterateWhileJust step
