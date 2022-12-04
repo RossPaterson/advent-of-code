ee88e45f9523c596df0e47dca48ff6220a0825de
Ross Paterson <R.Paterson@city.ac.uk>
2022-12-04 05:22:44 +0000

    cleanup

--- 2022/Day03.hs
+++ 2022/Day03.hs
@@ -8,21 +8,22 @@ import qualified Data.Set as Set
 -- Input processing
 
 type Input = [Rucksack]
-type Rucksack = (Compartment, Compartment)
-type Compartment = [Item]
+type Rucksack = [Item]
 type Item = Char
 
 parse :: String -> Input
-parse = map halves . lines
+parse = lines
+
+-- Part One
 
 halves :: [a] -> ([a], [a])
 halves xs = splitAt (length xs `div` 2) xs
 
--- Part One
-
 -- items in both compartments
 common :: Rucksack -> Set Item
-common (xs, ys) = Set.intersection (Set.fromList xs) (Set.fromList ys)
+common xs = Set.intersection (Set.fromList c1) (Set.fromList c2)
+  where
+    (c1, c2) = halves xs
 
 priority :: Item -> Int
 priority c
@@ -46,12 +47,9 @@ tests1 = [(testInput, 157)]
 
 -- Part Two
 
-contents :: Rucksack -> Set Item
-contents (xs, ys) = Set.union (Set.fromList xs) (Set.fromList ys)
-
 -- item in all the rucksacks
 badge :: [Rucksack] -> Item
-badge = head . Set.elems . foldr1 Set.intersection . map contents
+badge = head . Set.elems . foldr1 Set.intersection . map Set.fromList
 
 solve2 :: Input -> Int
 solve2 = sum . map (priority . badge) . takes 3
