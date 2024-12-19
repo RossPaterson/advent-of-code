97d9ed5a2b6d4982643387440014f25943e70580
Ross Paterson <R.Paterson@city.ac.uk>
2024-12-19 11:45:42 +0000

    comments

--- 2024/Day19.hs
+++ 2024/Day19.hs
@@ -27,21 +27,34 @@ solve1 :: Input -> Int
 solve1 (patterns, designs) =
     length (filter (possible patterns) designs)
 
+-- is it possible to assemble xs as a concatenation of elements of parts?
 possible :: Eq a => [[a]] -> [a] -> Bool
-possible ps xs = ways ps xs > 0
+possible parts xs = ways parts xs > 0
 
--- ways of assembling xs as a concatenation of elements of parts
+-- ways of assembling xs as a concatenation of elements of parts:
+-- memoization of ways_slow
 ways :: Eq a => [[a]] -> [a] -> Int
 ways parts xs = ways_from 0
   where
+    -- ways to assemble drop i xs
     ways_from i = ways_from_map!i
+    -- memo table for ways_from
     ways_from_map = Map.mapWithKey ways_from_aux tail_map
+    -- ways to assemble from_i = drop i xs
     ways_from_aux i from_i
       | null from_i = 1
       | otherwise =
         sum [ways_from (i + length p) | p <- parts, p `isPrefixOf` from_i]
     -- maps i to drop i xs
     tail_map = Map.fromList (zip [0..] (tails xs))
+
+-- ways of assembling xs as a concatenation of elements of parts:
+-- very slow direct implementation
+ways_slow :: Eq a => [[a]] -> [a] -> Int
+ways_slow parts xs
+  | null xs = 1
+  | otherwise =
+    sum [ways_slow parts (drop (length p) xs) | p <- parts, p `isPrefixOf` xs]
 
 testInput :: String
 testInput = "\
@@ -63,8 +76,7 @@ tests1 = [(testInput, 6)]
 -- Part Two
 
 solve2 :: Input -> Int
-solve2 (patterns, designs) =
-    sum (map (ways patterns) designs)
+solve2 (patterns, designs) = sum (map (ways patterns) designs)
 
 tests2 :: [(String, Int)]
 tests2 = [(testInput, 16)]
