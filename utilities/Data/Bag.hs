6991d39fd366603bd4789ed3c266d81182ab3ee3
Ross Paterson <R.Paterson@city.ac.uk>
2025-12-09 17:34:30 +0000

    extra bag ops

--- utilities/Data/Bag.hs
+++ utilities/Data/Bag.hs
@@ -5,10 +5,14 @@ module Data.Bag (
     empty,
     singleton,
     fromList,
+    fromCounts,
+    fromSet,
     -- * Queries
     size,
     count,
     counts,
+    elementSet,
+    entropy,
     -- * Combinations
     union,
     unions,
@@ -23,10 +27,15 @@ module Data.Bag (
 import Prelude hiding (map, concatMap)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
 
 -- | Bag or multiset of values
 newtype Bag a = Bag (Map a Int)
     deriving (Eq, Ord)
+
+instance (Enum a, Show a) => Show (Bag a) where
+    showsPrec p xs = showParen (p > 10) $
+        showString "fromCounts " . shows (counts xs)
 
 -- | Empty bag
 empty :: Bag a
@@ -40,6 +49,14 @@ singleton x = Bag (Map.singleton x 1)
 fromList :: Ord a => [a] -> Bag a
 fromList xs = Bag (Map.fromListWith (+) [(x, 1) | x <- xs])
 
+-- | Bag of elements with number of repetitions
+fromCounts :: Ord a => [(a, Int)] -> Bag a
+fromCounts xns = Bag (Map.fromListWith (+) [xn | xn@(_, n) <- xns, n > 0])
+
+-- | Bag of unique elements
+fromSet :: Ord a => Set a -> Bag a
+fromSet xs = Bag (Map.fromSet (const 1) xs)
+
 -- | Total number of elements
 size :: Bag a -> Int
 size (Bag xs) = sum (Map.elems xs)
@@ -52,7 +69,21 @@ count x (Bag xs) = Map.findWithDefault 0 x xs
 counts :: Bag a -> [(a, Int)]
 counts (Bag xs) = Map.assocs xs
 
--- | Bag union
+-- | Unique slements
+elementSet :: Bag a -> Set a
+elementSet (Bag xs) = Map.keysSet xs
+
+-- | The entropy (base 2) of the bag interpreted as a discrete probability
+-- distribution, ranging from 0 for a single value to \(\log_2 n\)
+-- for no repetitions.
+entropy :: Bag a -> Double
+entropy (Bag xs) =
+    (log n - sum [a*log a | x <- Map.elems xs, let a = fromIntegral x]/n)/log 2
+  where
+    n = fromIntegral (sum (Map.elems xs))
+
+-- | Bag union:
+-- @'count' a ('union' xs ys) = 'count' a xs + 'count' a ys@
 union :: Ord a => Bag a -> Bag a -> Bag a
 union (Bag xs) (Bag ys) = Bag (Map.unionWith (+) xs ys)
 
@@ -60,13 +91,15 @@ union (Bag xs) (Bag ys) = Bag (Map.unionWith (+) xs ys)
 unions :: Ord a => [Bag a] -> Bag a
 unions = foldr union empty
 
--- | Bag difference
+-- | Bag difference:
+-- @'count' a ('difference' xs ys) = max 0 ('count' a xs - 'count' a ys@)
 difference :: Ord a => Bag a -> Bag a -> Bag a
 difference (Bag xs) (Bag ys) =
     Bag (Map.difference xs ys `Map.union`
         Map.filter (>0) (Map.intersectionWith (-) xs ys))
 
--- | Bag intersection
+-- | Bag intersection:
+-- @'count' a ('intersection' xs ys) = min ('count' a xs) ('count' a ys)@)
 intersection :: Ord a => Bag a -> Bag a -> Bag a
 intersection (Bag xs) (Bag ys) = Bag (Map.intersectionWith min xs ys)
 
